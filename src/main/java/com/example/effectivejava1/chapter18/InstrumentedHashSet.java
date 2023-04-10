53bb88318b5b7325e85ac92d6838dabc99dc88d7
chickenchickenlove <ojt90902@naver.com>
2023-04-10 22:02:17 +0900

    effective java item 18 study

--- src/main/java/com/example/effectivejava1/chapter18/InstrumentedHashSet.java
+++ src/main/java/com/example/effectivejava1/chapter18/InstrumentedHashSet.java
@@ -1,2 +1,39 @@
-package com.example.effectivejava1.chapter18;public class InstrumentedHashSet {
+package com.example.effectivejava1.chapter18;
+
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.List;
+
+// 코드 18-1 잘못된 예 - 상속을 잘못 사용했다! (114쪽)
+public class InstrumentedHashSet <E> extends HashSet<E> {
+    // 추가된 원소의 수
+    private int addCount = 0;
+    public InstrumentedHashSet(){}
+
+    public InstrumentedHashSet(int initialCapacity, float loadFactor) {
+        super(initialCapacity, loadFactor);
+    }
+
+    @Override
+    public boolean add(E e) {
+        addCount++;
+        return super.add(e);
+    }
+
+    @Override
+    public boolean addAll(Collection<? extends E> c) {
+        addCount += c.size();
+        return super.addAll(c);
+    }
+
+    public int getAddCount() {
+        return addCount;
+    }
+
+    public static void main(String[] args) {
+        InstrumentedHashSet<String> s = new InstrumentedHashSet<String>();
+        s.addAll(List.of("틱", "탁탁", "펑"));
+        System.out.println(s.getAddCount());
+    }
+
 }
