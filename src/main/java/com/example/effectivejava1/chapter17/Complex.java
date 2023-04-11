7c3e8abcf886015c7c6d262dfc515b90fa772c10
chickenchickenlove <ojt90902@naver.com>
2023-04-11 23:35:05 +0900

    study effective java item 17

---
+++ src/main/java/com/example/effectivejava1/chapter17/Complex.java
@@ -0,0 +1,44 @@
+package com.example.effectivejava1.chapter17;
+
+// 코드 17-1 불변 복소수 클래스 (106-107쪽)
+public class Complex {
+
+    private final double re;
+    private final double im;
+
+    public static final Complex ZERO = new Complex(0, 0);
+    public static final Complex ONE = new Complex(1, 0);
+    public static final Complex I = new Complex(0, 1);
+
+    public Complex(double re, double im) {
+        this.re = re;
+        this.im = im;
+    }
+
+    public double realPart() {return re;}
+    public double imaginaryPart() {return im;}
+
+    public Complex plus(Complex c) {
+        return new Complex(re + c.re, im + c.im);
+    }
+
+    // 코드 17-2 정적 팩터리 (private 생성자와 함께 사용해야 한다.) (110-111쪽)
+    public static Complex valueOf(double re, double im) {
+        return new Complex(re, im);
+    }
+
+
+    public Complex minus(Complex c) {
+        return new Complex(re - c.re, im - c.im);
+    }
+
+    public Complex times(Complex c) {
+        return new Complex(re * c.re - im*c.im, re * c.im + im * c.re);
+    }
+
+    public Complex dividedBy(Complex c) {
+        double tmp = c.re * c.re + c.im * c.im;
+        return new Complex((re * c.re + im * c.im) / tmp,
+                (im * c.re - re * c.im) / tmp);
+    }
+}
