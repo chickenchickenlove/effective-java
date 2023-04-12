ea43b35a2ac43cf856de3b65c575ef6d933b3c91
chickenchickenlove <ojt90902@naver.com>
2023-04-12 22:06:07 +0900

    effective java chatper 22 study

--- src/main/java/com/example/effectivejava1/chapter22/MyClass.java
+++ src/main/java/com/example/effectivejava1/chapter22/MyClass.java
@@ -1,12 +1,8 @@
 package com.example.effectivejava1.chapter22;
 
 // 코드 22-1 상수 인터페이스 안티패턴 - 사용금지! (139쪽)
-public class PhysicalConstants {
-
-    static final double AVOGADROS_NUMBER = 6.022_140_857e23;
-    static final double BOLTZMANN_CONSTANT = 1.380_648_52e-23;
-    static final double ELECTRON_MASS = 9.109_383_56e-31;
-
+public class MyClass implements PhysicalConstants{
+    public static void main(String[] args) {
+        PhysicalConstants myClass = new MyClass();
+    }
 }
-
-
--- src/main/java/com/example/effectivejava1/chapter22/utilconstant/PhysicalConstants.java
+++ src/main/java/com/example/effectivejava1/chapter22/utilconstant/PhysicalConstants.java
@@ -1,11 +1,14 @@
-package com.example.effectivejava1.chapter22;
+package com.example.effectivejava1.chapter22.utilconstant;
 
-// 코드 22-1 상수 인터페이스 안티패턴 - 사용금지! (139쪽)
-public interface PhysicalConstants {
+// 코드 22-2 상수 유틸리티 클래스
+public final class PhysicalConstants {
 
-    static final double AVOGADROS_NUMBER = 6.022_140_857e23;
-    static final double BOLTZMANN_CONSTANT = 1.380_648_52e-23;
-    static final double ELECTRON_MASS = 9.109_383_56e-31;
+    private PhysicalConstants() {
+    }
+
+    public static final double AVOGADROS_NUMBER = 6.022_140_857e23;
+    public static final double BOLTZMANN_CONSTANT = 1.380_648_52e-23;
+    public static final double ELECTRON_MASS = 9.109_383_56e-31;
 
 }
 
