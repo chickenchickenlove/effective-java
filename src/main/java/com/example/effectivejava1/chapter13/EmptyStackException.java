31c402ba5bf6c70c198b1f57aedc560c95c5c013
chickenchickenlove <ojt90902@naver.com>
2023-05-17 23:46:37 +0900

    effective java item13 study

---
+++ src/main/java/com/example/effectivejava1/chapter13/EmptyStackException.java
@@ -0,0 +1,4 @@
+package com.example.effectivejava1.chapter13;
+
+public class EmptyStackException extends IllegalStateException {
+}
---
+++ src/main/java/com/example/effectivejava1/chapter13/exception/MyException.java
@@ -0,0 +1,4 @@
+package com.example.effectivejava1.chapter13.exception;
+
+public class MyException extends Exception {
+}
